39c9d8e6bfeff2b8b0be5d33a95b89c73d04e809
Tom Delaney <delaneyt1@gmail.com>
2025-04-02 01:53:34 -0400

    🧪 Add failing test for relative path
    Add a test that ensures config with relative path is relative to entry assembly

---
+++ src/Management/test/Endpoint.Test/Actuators/Logfile/LogfileEndpointTest.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Reflection;
+using Microsoft.AspNetCore.Routing.Matching;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace Steeltoe.Management.Endpoint.Test.Actuators.Logfile;
+
+public sealed class LogfileEndpointTest(ITestOutputHelper testOutputHelper) : BaseTest
+{
+    private readonly ITestOutputHelper _testOutputHelper = testOutputHelper;
+
+    [Fact]
+    public void GetLogFilePathWithRelativePath_ReturnsExpected()
+    {
+        // arrange
+        string expectedFilePath = Path.Combine(Assembly.GetEntryAssembly()!.Location, "logs/testfile.log");
+        var appSettings = new Dictionary<string, string?>
+        {
+            ["management:endpoints:logfile:filePath"] = "logs/testfile.log",
+            ["management:endpoints:logfile:enabled"] = "true"
+        };
+
+        using var testContext = new TestContext(_testOutputHelper);
+
+        testContext.AdditionalServices = (services, _) =>
+        {
+            services.AddSingleton(TestHostEnvironmentFactory.Create());
+            services.AddLogfileActuator();
+        };
+
+        testContext.AdditionalConfiguration = configuration =>
+        {
+            configuration.AddInMemoryCollection(appSettings);
+        };
+
+        var handler = (LogfileEndpointHandler)testContext.GetRequiredService<ILogfileEndpointHandler>();
+
+        // act
+        string result = handler.GetLogFilePath();
+
+        // assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedFilePath, result);
+    }
+}
